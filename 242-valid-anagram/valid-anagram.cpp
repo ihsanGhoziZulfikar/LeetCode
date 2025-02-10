ec4096245c46ae922c6f0f571d92e51f2fac8e2b
ihsanGhoziZulfikar <100748074+ihsanGhoziZulfikar@users.noreply.github.com>
2025-02-10 11:22:52 +0700

    Time: 7 ms (22.97%) | Memory: 9.8 MB (48.19%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())return false;
+        map<char, int> words;
+        for(int i=0;i<s.length();i++){
+            auto it=words.find(s[i]);
+            if(it==words.end()){
+                words[s[i]]=1;
+            }else{
+                words[s[i]]++;
+            }
+        }
+        for(int i=0;i<t.length();i++){
+            auto it=words.find(t[i]);
+            if(it==words.end() || it->second <=0){
+                return false;
+            }else{
+                words[t[i]]--;
+            }
+        }
+        return true;
+    }
+};
