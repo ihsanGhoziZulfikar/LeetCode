1aa2949cccfb1801b9d8e988fa91b74de7cb0ac5
ihsanGhoziZulfikar <100748074+ihsanGhoziZulfikar@users.noreply.github.com>
2025-03-25 17:50:14 +0700

    Time: 0 ms (100.00%) | Memory: 115.6 MB (80.69%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,44 @@
+typedef long long int ll;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        ll xx=2147483648;
+        ll n1=xx;
+        ll n2=xx;
+        ll c1=xx;
+        ll c2=xx;
+
+        for(int i=0;i<nums.size();i++){
+            // cout << nums[i] << " " << n2 << endl;
+            if(nums[i]>n2){
+                return true;
+            }
+            if(n2==xx){
+                if(n1<nums[i]){
+                    n2=nums[i];
+                }else{
+                    n1=nums[i];
+                }
+                continue;
+            }
+            if(n1<nums[i] && n2>nums[i]){
+                n2=nums[i];
+                continue;
+            }
+            if(c1>=nums[i]){
+                // cout << "c1 ";
+                c1=nums[i];
+                continue;
+            }
+            if(n2>nums[i]){
+                // cout << "last ";
+                n1=c1;
+                n2=nums[i];
+                continue;
+            }
+            
+        }
+        return false;
+    }
+};
