54b3c7bf05320694aa49f6663a2d46f4aaa9bf79
ihsanGhoziZulfikar <100748074+ihsanGhoziZulfikar@users.noreply.github.com>
2025-06-02 11:12:50 +0700

    Time: 0 ms (100.00%) | Memory: 10.2 MB (68.53%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -3,7 +3,6 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int left=0,right=matrix[0].size()-1,top=0,bottom=matrix.size()-1;
         while(left<right && top<bottom){
-            cout << left << " " << right << endl;
             for(int i=0;i<right-left;i++){
                 int temp = matrix[top][left+i];
                 matrix[top][left+i] = matrix[bottom-i][left];
@@ -16,26 +15,5 @@ public:
             top++;
             bottom--;
         }
-            // for(int i=0;i<matrix.size()-1;i++){
-            //     int temp = matrix[top+0][left+i];
-            //     matrix[top+0][left+i] = matrix[bottom-i][left+0];         // left right   (top)
-            //     matrix[bottom-i][left+0] = matrix[bottom-0][right-i];     //bottom up     (left)
-            //     matrix[bottom-0][right-i] = matrix[top+i][right-0];     //right left    (bottom)
-            //     matrix[top+i][right-0] = temp;                          //top bottom    (right)
-            // }
     }
 };
-
-// class Solution {
-// public:
-//     void rotate(vector<vector<int>>& matrix) {
-//         int left=0,right=matrix[0].size()-1,top=0,bottom=matrix.size()-1;
-//             for(int i=0;i<matrix.size()-1;i++){
-//                 int temp = matrix[top+0][left+i];
-//                 matrix[top+0][left+i] = matrix[bottom-i][left+0];         // left right   (top)
-//                 matrix[bottom-i][left+0] = matrix[bottom-0][right-i];     //bottom up     (left)
-//                 matrix[bottom-0][right-i] = matrix[top+i][right-0];     //right left    (bottom)
-//                 matrix[top+i][right-0] = temp;                          //top bottom    (right)
-//             }
-//     }
-// };
