c817eb1cb942fa89905327c0f52c311b28ad4287
ihsanGhoziZulfikar <100748074+ihsanGhoziZulfikar@users.noreply.github.com>
2025-06-10 10:09:36 +0700

    Time: 0 ms (100.00%) | Memory: 18.7 MB (24.52%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        set<int> si;
+        set<int> sj;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[i].size();j++){
+                if(matrix[i][j] == 0){
+                    si.insert(i);
+                    sj.insert(j);
+                }
+            }
+        }
+        
+        for(auto it=si.begin();it!=si.end();++it){
+            cout << *it << endl;
+            for(int j=0;j<matrix[0].size();j++){
+                matrix[*it][j] = 0;
+            }
+            // for(int j=0;j<matrix[0].size();j++){
+            //     matrix[*it][j] = 0;
+            // }
+        }
+        for(auto it=sj.begin();it!=sj.end();++it){
+            cout << *it << endl;
+            for(int j=0;j<matrix.size();j++){
+                matrix[j][*it] = 0;
+            }
+        }
+    }
+};
